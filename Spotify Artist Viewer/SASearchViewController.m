8d4e263b7873064be2098de2365eaff7059c970b
Alan Scarpa <alan@intrepid.io>
2015-09-30 16:17:25 -0400

    Collection view being constrained with PureLayout FINALLY

--- Spotify Artist Viewer/SASearchViewController.m
+++ Spotify Artist Viewer/SASearchViewController.m
@@ -14,15 +14,17 @@
 #import "SASearchTableViewCell.h"
 #import "SASearchTableViewCell+SASearchCellCustomizer.h"
 #import "SAInfiniteScrollHandler.h"
+#import <PureLayout/PureLayout.h>
 
 static NSInteger const kReturnLimit = 3;
 
-@interface SASearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
+@interface SASearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) NSMutableArray *artistsFromSearch;
 @property (strong, nonatomic) SAInfiniteScrollHandler *infiniteScrollHandler;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 @property (nonatomic, strong) NSMutableArray *tableViewConstraints;
+@property (nonatomic, strong) UICollectionView *collectionView;
 @end
 
 @implementation SASearchViewController
@@ -32,7 +34,8 @@ static NSInteger const kReturnLimit = 3;
     [self prepareArtistsArray];
     [self setSearchBarDelegate];
     [self setUpInfiniteScroll];
-    self.tableViewConstraints = [[NSMutableArray alloc]init];
+    [self saveTableViewStoryboardConstraints];
+    [self prepareCollectionView];
 }
 
 - (void)prepareArtistsArray {
@@ -48,23 +51,56 @@ static NSInteger const kReturnLimit = 3;
     [self.infiniteScrollHandler setUpInfiniteScrollOnViewController:self withSearchLimit:kReturnLimit];
 }
 
+- (void)saveTableViewStoryboardConstraints {
+    self.tableViewConstraints = [[NSMutableArray alloc]init];
+    for (NSLayoutConstraint *constraint in self.view.constraints){
+        if (constraint.firstItem == self.tableView || constraint.secondItem == self.tableView){
+            [self.tableViewConstraints addObject:constraint];
+        }
+    }
+}
+
+- (void)prepareCollectionView {
+    UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+    self.collectionView = [[UICollectionView alloc]initWithFrame:self.view.frame collectionViewLayout:layout];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"collectionViewCell"];
+    self.collectionView.backgroundColor = [UIColor orangeColor];
+}
+
 - (IBAction)segmedControlTapped:(id)sender {
+    [self checkWhichSegmentWasTapped];
+}
+
+- (void)checkWhichSegmentWasTapped {
     if (self.segmentedControl.selectedSegmentIndex == 0){
-        NSLog(@"List");
-        [self.view addSubview:self.tableView];
-        for (NSLayoutConstraint *constraint in self.tableViewConstraints){
-            [self.view addConstraint:constraint];
-        }
+        [self showTableView];
     } else {
-        NSLog(@"Collection");
-        for (NSLayoutConstraint *constraint in self.view.constraints){
-            if (constraint.firstItem == self.tableView || constraint.secondItem == self.tableView){
-              [self.tableViewConstraints addObject:constraint];
-            }
-        }
-        
-        [self.tableView removeFromSuperview];
+        [self showCollectionView];
     };
+}
+
+- (void)showTableView {
+    [self.collectionView removeFromSuperview];
+    [self.view addSubview:self.tableView];
+    for (NSLayoutConstraint *constraint in self.tableViewConstraints){
+        [self.view addConstraint:constraint];
+    }
+}
+
+- (void)showCollectionView {
+    [self.tableView removeFromSuperview];
+    [self.view addSubview:self.collectionView];
+    [self constrainCollectionView];
+}
+
+- (void)constrainCollectionView {
+    [self.collectionView autoPinEdge:ALEdgeTop toEdge:ALEdgeBottom ofView:self.searchBar];
+    [self.collectionView autoPinEdge:ALEdgeBottom toEdge:ALEdgeBottom ofView:self.view];
+    [self.collectionView autoPinEdge:ALEdgeLeft toEdge:ALEdgeLeft ofView:self.view];
+    [self.collectionView autoPinEdge:ALEdgeRight toEdge:ALEdgeRight ofView:self.view];
+    [super updateViewConstraints];
 }
 
 #pragma mark - Search function
@@ -114,6 +150,61 @@ static NSInteger const kReturnLimit = 3;
     [cell customizeCellWithArtist:self.artistsFromSearch[indexPath.row]];
     return cell;
 }
+
+#pragma mark UICollectionViewDataSource
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 24;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"collectionViewCell" forIndexPath:indexPath];
+    
+    // Configure the cell
+    cell.backgroundColor = [UIColor greenColor];
+    return cell;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(self.view.frame.size.width/2.5,self.view.frame.size.height/4.5);
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+ // Uncomment this method to specify if the specified item should be highlighted during tracking
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+ }
+ */
+
+/*
+ // Uncomment this method to specify if the specified item should be selected
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+ return YES;
+ }
+ */
+
+/*
+ // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+ }
+ 
+ - (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+ }
+ 
+ - (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+ }
+ */
+
 
 #pragma mark - Navigation
 
